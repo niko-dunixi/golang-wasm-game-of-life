9078caf723e69ffe49e5fb010baa61b4e445a7d9
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-04-22 14:36:49 -0600

    Adding a canvas and resizing it automatically

--- client/main.go
+++ client/main.go
@@ -2,18 +2,56 @@
 
 package main
 
+import (
+	"fmt"
+	"syscall/js"
+)
+
+var (
+	messages   chan string
+	window     = js.Global()
+	canvas     js.Value
+	context    js.Value
+	windowSize struct{ width, height float64 }
+)
+
 func main() {
-	messages := make(chan string)
+	messages = make(chan string)
 	go func() {
 		for message := range messages {
 			println(message)
 		}
 	}()
 	messages <- "WASM::main"
-
-	
+	setupCanvas()
 
 	messages <- "WASM::This will now run forever!"
 	runForever := make(chan bool)
 	<-runForever
+}
+
+func setupCanvas() {
+	messages <- "WASM::setupCanvas"
+	document := window.Get("document")
+	canvas = document.Call("createElement", "canvas")
+
+	body := document.Get("body")
+	body.Call("appendChild", canvas)
+
+	context = canvas.Call("getContext", "2d")
+
+	updateWindowSizeJSCallback := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resetWindowSize()
+		return nil
+	})
+	window.Call("addEventListener", "resize", updateWindowSizeJSCallback)
+	resetWindowSize()
+}
+
+func resetWindowSize() {
+	windowSize.width = window.Get("innerWidth").Float()
+	windowSize.height = window.Get("innerHeight").Float()
+	canvas.Set("width", windowSize.width)
+	canvas.Set("height", windowSize.height)
+	messages <- fmt.Sprintf("WASM::resetWindowSize (%f x %f)", windowSize.width, windowSize.height)
 }
