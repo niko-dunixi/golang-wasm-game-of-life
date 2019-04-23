497e4a9c70a2f6b9486d2798af536d8c2979ad3b
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-04-23 13:37:33 -0600

    Now including serverside code.

---
+++ server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	fmt.Println(os.Getwd())
+	http.HandleFunc("/", IndexHtml)
+	http.HandleFunc("/wasm_exec.js", WasmExecJs)
+	http.HandleFunc("/client.wasm", ClientWasm)
+
+	srv := &http.Server{
+		Addr: ":4141",
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Running forever until exit requested")
+	<-stop
+	log.Println("Exit requested. Now halting.")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func IndexHtml(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	bytes, _ := ioutil.ReadFile("./bin/index.html")
+	_, _ = w.Write(bytes)
+}
+
+func WasmExecJs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	bytes, _ := ioutil.ReadFile("./bin/wasm_exec.js")
+	_, _ = w.Write(bytes)
+}
+
+func ClientWasm(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/wasm")
+	bytes, _ := ioutil.ReadFile("./bin/client.wasm")
+	_, _ = w.Write(bytes)
+}
