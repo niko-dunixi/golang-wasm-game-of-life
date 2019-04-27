0c427ae5768c869ba1f6e6cec81a6717b6d24a22
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-04-27 11:46:44 -0600

    Simple controlls. Need to have the App class control the state, but the WASM does a lot of that, which it really shouldn't.

--- react-ui/src/GameControls.js
+++ react-ui/src/GameControls.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import GameControl from './GameControl'
 
-function GameControls() {
-    return (
-        <div>
-            PLAY
-        </div>
-    );
+class GameControls extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <header>controls:</header>
+                <GameControl action={this.handleClick} icon={'>>'}></GameControl>
+            </div>
+        );
+    }
+
+    handleClick(event) {
+        alert(event.toString());
+    }
 }
 
 export default GameControls;
