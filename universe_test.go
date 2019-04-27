2468022580cc6c106adf3192c4a763173159e29d
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-04-27 06:09:05 -0600

    Adding unit test, currently failing

--- universe_test.go
+++ universe_test.go
@@ -13,11 +13,30 @@ var (
 func TestNewBufferedUniverse(t *testing.T) {
 	expectedRows := r.Intn(30)
 	expectedColumns := r.Intn(30)
-	u := NewBufferedUniverse(expectedRows, expectedColumns, NewRand(1))
+	u := NewBufferedUniverse(expectedRows, expectedColumns, func(b *[]bool) {})
 	if u.RowCount() != expectedRows {
 		t.Errorf("expected rows should be %d, but was %d", expectedRows, u.RowCount())
 	}
 	if u.ColumnCount() != expectedColumns {
 		t.Errorf("expected columns should be %d, but was %d", expectedColumns, u.ColumnCount())
+	}
+}
+
+func TestBufferedUniverse_IsAlive(t *testing.T) {
+	// Still life:
+	//   0000
+	//   0XX0
+	//   0XX0
+	//   0000
+	u := NewBufferedUniverse(4, 4, func(b *[]bool) {
+		bools := *b
+		bools[6] = true
+		bools[7] = true
+		bools[10] = true
+		bools[11] = true
+	})
+	if u.IsDead(1, 1) || u.IsDead(1, 2) ||
+		u.IsDead(2, 1) || u.IsDead(2, 2) {
+		t.Errorf("Cells weren't alive: %s", u)
 	}
 }
