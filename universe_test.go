a52215e7dac942e0c4edafa3324a96f7b5972e2e
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-04-27 06:38:00 -0600

    Better testing

--- universe_test.go
+++ universe_test.go
@@ -23,25 +23,56 @@ func TestNewBufferedUniverse(t *testing.T) {
 }
 
 func TestBufferedUniverse_IsAlive(t *testing.T) {
+	// Single Corner:
+	//   0000
+	//   0000
+	//   0000
+	//   000X
+	u := NewBufferedUniverse(4, 4, func(b *[]bool) {
+		bools := *b
+		bools[len(bools)-1] = true
+	})
+	for currentRow := 0; currentRow < u.RowCount(); currentRow++ {
+		for currentColumn := 0; currentColumn < u.ColumnCount(); currentColumn++ {
+			isLastCell := currentColumn+1 == u.ColumnCount() && currentRow+1 == u.RowCount()
+			if isLastCell {
+				if u.IsDead(currentRow, currentColumn) {
+					t.Fatalf("Corner cell should be alive\n%s", u)
+				}
+			} else {
+				if u.IsAlive(currentRow, currentColumn) {
+					t.Fatalf("Only far corner cell should be alive\n%s", u)
+				}
+			}
+		}
+	}
+	if u.IsDead(-1, -1) {
+		t.Errorf("Didn't calculate opposite corner correctly\n%s", u)
+	}
+}
+
+func TestBufferedUniverse_Iterate(t *testing.T) {
 	// Still life:
 	//   0000
 	//   0XX0
 	//   0XX0
 	//   0000
-	u := NewBufferedUniverse(4, 4, func(b *[]bool) {
+	stillLife := NewBufferedUniverse(4, 4, func(b *[]bool) {
 		bools := *b
 		bools[5] = true
 		bools[6] = true
 		bools[9] = true
 		bools[10] = true
 	})
-	if u.IsDead(1, 1) || u.IsDead(1, 2) ||
-		u.IsDead(2, 1) || u.IsDead(2, 2) {
-		t.Errorf("Cells weren't alive\n%s", u)
+	if stillLife.IsDead(1, 1) || stillLife.IsDead(1, 2) ||
+		stillLife.IsDead(2, 1) || stillLife.IsDead(2, 2) {
+		t.Errorf("StillLife didn't initialize correctly\n%s", stillLife)
 	}
-}
-
-func TestBufferedUniverse_Iterate(t *testing.T) {
+	stillLife.Iterate()
+	if stillLife.IsDead(1, 1) || stillLife.IsDead(1, 2) ||
+		stillLife.IsDead(2, 1) || stillLife.IsDead(2, 2) {
+		t.Errorf("StillLife didn't iterate correctly\n%s", stillLife)
+	}
 	// Oscillator Ia:
 	//   00000
 	//   00X00
@@ -54,17 +85,17 @@ func TestBufferedUniverse_Iterate(t *testing.T) {
 	//   0XXX0
 	//   00000
 	//   00000
-	oscillatorOne := NewBufferedUniverse(5, 5, func(b *[]bool) {
+	oscillatorI := NewBufferedUniverse(5, 5, func(b *[]bool) {
 		bools := *b
 		bools[7] = true
 		bools[12] = true
 		bools[17] = true
 	})
-	if oscillatorOne.IsDead(1, 2) || oscillatorOne.IsDead(2, 2) || oscillatorOne.IsDead(3, 2) {
-		t.Errorf("Cells weren't alive\n%s", oscillatorOne)
+	if oscillatorI.IsDead(1, 2) || oscillatorI.IsDead(2, 2) || oscillatorI.IsDead(3, 2) {
+		t.Errorf("Oscillator I didn't initialize correctly\n%s", oscillatorI)
 	}
-	oscillatorOne.Iterate()
-	if oscillatorOne.IsDead(2, 1) || oscillatorOne.IsDead(2, 2) || oscillatorOne.IsDead(2, 3) {
-		t.Errorf("Cells didn't iterate correctly\n%s", oscillatorOne)
+	oscillatorI.Iterate()
+	if oscillatorI.IsDead(2, 1) || oscillatorI.IsDead(2, 2) || oscillatorI.IsDead(2, 3) {
+		t.Errorf("Oscillator I didn't iterate correctly\n%s", oscillatorI)
 	}
 }
